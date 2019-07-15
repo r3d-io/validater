e4ae0b519ca14cfeac46e9d50b0253fd15aaadad
Anuj singh <askoli@techracers.io>
2019-07-15 16:21:38 +0530

    Added get transaction receipt

--- schema.js
+++ schema.js
@@ -57,6 +57,60 @@ const getTransaction = {
   ]
 }
 
+const getTransactionReceipt = {
+  "type": "object",
+  "properties": {
+    "blockHash": {
+      "type": "string"
+    },
+    "blockNumber": {
+      "type": "integer"
+    },
+    "contractAddress": {
+      "type": "null"
+    },
+    "cumulativeGasUsed": {
+      "type": "integer"
+    },
+    "from": {
+      "type": "string"
+    },
+    "gasUsed": {
+      "type": "integer"
+    },
+    "logs": {
+      "type": "array",
+      "items": {}
+    },
+    "logsBloom": {
+      "type": "string"
+    },
+    "status": {
+      "type": "boolean"
+    },
+    "to": {
+      "type": "string"
+    },
+    "transactionHash": {
+      "type": "string"
+    },
+    "transactionIndex": {
+      "type": "integer"
+    }
+  },
+  "required": [
+    "blockHash",
+    "from",
+    "gasUsed",
+    "logs",
+    "status",
+    "to",
+    "transactionHash",
+    "transactionIndex"
+  ]
+}
+
 module.exports = {
-  getTransaction
+  getTransaction,
+  getTransactionReceipt
 }
--- validater.js
+++ validater.js
@@ -1,14 +1,16 @@
 let data = require("./example");
 let schemas = require("./schema");
+const chalk = require('chalk');
 
 let Ajv = require('ajv');
 const ajv = new Ajv({ allErrors: true });
 
-function validateJson(schema,data) {
+function validateJson(schema, data, name) {
   let validate = ajv.compile(schema);
   let valid = validate(data);
   if (!valid) console.log(validate.errors);
-  else console.log("Valid data")
+  else console.log("Valid data",chalk.green(name))
 }
 
-validateJson(schemas.getTransaction, data.getTransaction)
+validateJson(schemas.getTransaction, data.getTransaction, "get tranasction")
+validateJson(schemas.getTransactionReceipt, data.getTransactionReceipt, "get transaction receipt")
